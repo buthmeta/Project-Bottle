add482d4742f244507c25a40b2cbfa66300c855e
apple <apple@appleui-iMac.bbrouter>
2025-02-19 11:49:33 +0700

    FrontEnd

---
+++ FrontEnd/src/Data.js
@@ -0,0 +1,12 @@
+// const search = document.querySelector(".search");
+
+// document.querySelector("#search_icon").onclick = () => {
+//   search.classList.toggle("active");
+//   user.classList.remove("active");
+// };
+
+// const user = document.querySelector(".user");
+// document.querySelector("#user_icon").onclick = () => {
+//   user.classList.toggle("active");
+//   search.classList.remove("active");
+// };
